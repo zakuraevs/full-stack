9e5d537b5d077cdc9b08182d7ffb150a0a0ed0a8
zakuraevs <zakuraevs@gmail.com>
2020-06-20 04:12:41 +0300

    Added tasks 1.7 - 1.9

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -10,7 +10,6 @@ const Button = ({onClick, text}) => {
 }
 
 const Label = ({parameter, value}) => {
-
   return (
     <div>
       {parameter} {value}
@@ -18,21 +17,59 @@ const Label = ({parameter, value}) => {
   )  
 }
 
+const Statistics = ({good, neutral, bad, all, sum}) => {
+
+  const division = (a, b) => {
+    if(b === 0) return 0
+    return a/b 
+  }
+
+  const getPercent = (value) => {
+    return value * 100 + ' %'
+  }
+
+  if(all === 0) return( 
+    <div>
+      No feedback given
+    </div>
+  )
+
+
+  return (
+    <div>
+      <Label parameter={'good'} value={good} />
+      <Label parameter={'neutral'} value={neutral} />
+      <Label parameter={'bad'} value={bad} />
+      <Label parameter={'all'} value={all} />
+      <Label parameter={'average'} value={division(sum, all)} />
+      <Label parameter={'positive'} value={getPercent(division(good, all)) } />
+    </div>
+  )
+}
+
 const App = () => {
+  
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [sum, setAverage] = useState(0)
 
   const registerGood = () => {
     setGood(good + 1)
+    setAll(all + 1)
+    setAverage(sum + 1)
   }
 
   const registerNeutral = () => {
     setNeutral(neutral + 1)
+    setAll(sum + 1)
   }
 
   const registerBad = () => {
     setBad(bad + 1)
+    setAll(all + 1)
+    setAverage(sum - 1)
   }
 
   return (
@@ -42,9 +79,7 @@ const App = () => {
       <Button onClick={registerNeutral} text='neutral' />
       <Button onClick={registerBad} text='bad' />
       <h1>statistics</h1>
-      <Label parameter={'good'} value={good} />
-      <Label parameter={'neutral'} value={neutral} />
-      <Label parameter={'bad'} value={bad} />
+      <Statistics good={good} neutral ={neutral} bad={bad} all={all} sum={sum} />
     </div>
   )
 }
