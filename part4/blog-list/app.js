467df7adc0a294462f841f43d8f90659a1a2ff08
zakuraevs <zakuraevs@gmail.com>
2020-07-19 06:35:45 +0300

    Added part 4.15: bloglist expansion step 3

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -6,6 +6,8 @@ const cors = require('cors')
 const logger = require('./utils/logger')
 const blogsRouter = require('./controllers/blogs')
 const middleware = require('./utils/middleware')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 mongoose.set('useFindAndModify', false)
 
@@ -22,6 +24,8 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
--- part4/blog-list/controllers/blogs.js
+++ part4/blog-list/controllers/blogs.js
@@ -1,37 +1,57 @@
 const blogsRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
-//blogsRouter.get('/', (request, response) => {
-//    response.send('<h1>Phonebook app <3</h1>')
-//  })
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+        return authorization.substring(7)
+    }
+    return null
+}
 
 blogsRouter.get('/', async (request, response) => {
 
-    const blogs = await Blog.find({})
+    const blogs = await Blog
+        .find({}).populate('user', { username: 1, name: 1 })
 
     response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
+    const body = request.body
+    const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
 
-    const blog = new Blog(request.body)
+    if (!token || !decodedToken.id) {
+        return response.status(401).json({ error: 'token missing or invalid' })
+    }
+
+    const user = await User.findById(decodedToken.id)
+    //not sure this will work
+    //edit: it works!!!
+    const blog = new Blog({ ...body, user: user._id })
 
     //checking if likes are given
     //if not, defaulting to zero
-    if(!('likes' in request.body)) {
+    if (!('likes' in request.body)) {
         console.log('no likes were given, defaulting to 0')
         blog.likes = 0
     }
 
     //checking if author & url are given
     //if not responding with 400 Bad Request
-    if(!('author' in request.body) && !('url' in request.body)) {
+    if (!('author' in request.body) && !('url' in request.body)) {
         console.log('author & url missing from request')
         response.status(400).end()
         return
     }
 
     const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+
     response.json(savedBlog)
 })
 
@@ -53,6 +73,6 @@ blogsRouter.put('/:id', async (request, response) => {
     const updated = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
 
     response.json(updated)
-  })
+})
 
 module.exports = blogsRouter
