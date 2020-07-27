980ca8af02be2168d0fb2c0eb75c7e366f3555dc
zakuraevs <zakuraevs@gmail.com>
2020-07-27 09:26:50 +0300

    Added part6.19

--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch, useSelector, connect } from 'react-redux'
 import { incrementVotes } from '../reducers/anecdoteReducer'
 import { addNotification } from '../reducers/notificationReducer'
 import { removeNotification } from '../reducers/notificationReducer'
@@ -20,26 +20,25 @@ const Anecdote = ({ anecdote, handleVote }) => {
   )
 }
 
-const AnecdoteList = () => {
+const AnecdoteList = (props) => {
   const dispatch = useDispatch()
-  const filterValue = store.getState().filter
-  const anecdotes = useSelector(state => state.anecdotes
+  //const filterValue = store.getState().filter
+  /*const anecdotes = useSelector(state => state.anecdotes
     .sort((a, b) => (a.votes > b.votes) ? -1 : 1)
     .filter(anecdote => anecdote.content.includes(filterValue))
-
-  )
+  )*/
 
   return (
     <ul>
-      {anecdotes.map(anecdote =>
+      {props.anecdotes.map(anecdote =>
         <Anecdote
           key={anecdote.id}
           anecdote={anecdote}
-          handleVote={ async () => {
-              await anecdoteService.updateAnecdote(anecdote.content, anecdote.votes + 1, anecdote.id)
-              dispatch(incrementVotes(anecdote.id))
-              dispatch(addNotification(`you voted '${anecdote.content}'`, 5))
-            }
+          handleVote={async () => {
+            await anecdoteService.updateAnecdote(anecdote.content, anecdote.votes + 1, anecdote.id)
+            dispatch(incrementVotes(anecdote.id))
+            dispatch(addNotification(`you voted '${anecdote.content}'`, 5))
+          }
           }
         />
       )}
@@ -47,4 +46,18 @@ const AnecdoteList = () => {
   )
 }
 
-export default AnecdoteList
+const mapStateToProps = (state) => {
+  // sometimes it is useful to console log from mapStateToProps
+  console.log(state)
+  const anecdotes = state.anecdotes
+    .sort((a, b) => (a.votes > b.votes) ? -1 : 1)
+    .filter(anecdote => anecdote.content.includes(state.filter))
+
+  return {
+    anecdotes: anecdotes,
+    filter: state.filter
+  }
+}
+
+const ConnectedNAnecdotes = connect(mapStateToProps)(AnecdoteList)
+export default ConnectedNAnecdotes
