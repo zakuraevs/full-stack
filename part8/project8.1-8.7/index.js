3895b5b57b9b6fb1e6f34cbe65bad38d60245fb1
zakuraevs <zakuraevs@gmail.com>
2020-08-04 21:52:03 +0300

    Added part8.3

--- part8/project8.1-8.7/index.js
+++ part8/project8.1-8.7/index.js
@@ -88,10 +88,18 @@ const typeDefs = gql`
     genres: [String!]
   }
 
+  type Author {
+    name: String!
+    id: ID!
+    born: Int
+    bookCount: Int!
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `
 
@@ -99,9 +107,15 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books
+    allBooks: () => books,
+    allAuthors: () => authors
+  },
+  Author: {
+    bookCount: (root) => {
+      console.log(root)
+      return books.filter(b => b.author === root.name).length
+    }
   }
-
 }
 
 const server = new ApolloServer({
